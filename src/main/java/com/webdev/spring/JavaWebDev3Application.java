76080c0200380f7c35c6bb1e0ceaed71338057fc
Seungil <seungilbang@khu.ac.kr>
2023-05-05 02:41:43 +0900

    initial commit

---
+++ src/main/java/com/webdev/spring/JavaWebDev3Application.java
@@ -0,0 +1,13 @@
+package com.webdev.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebDev3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebDev3Application.class, args);
+	}
+
+}
