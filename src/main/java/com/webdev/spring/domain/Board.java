d880db6ed30ae33248409494e6655cc3a11bf791
Bang <seungilbang@khu.ac.kr>
2023-05-10 19:31:51 +0900

    [ADD] Board Entity

---
+++ src/main/java/com/webdev/spring/domain/Board.java
@@ -0,0 +1,31 @@
+package com.webdev.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    @Column(length = 200, nullable = false)
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    @Column(length = 50, nullable = false)
+    private String writer;
+
+    public void change(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
