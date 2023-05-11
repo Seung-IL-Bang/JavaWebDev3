b44391cf85327ca5114cfc539e7a0b7256411fa4
Seungil <seungilbang@khu.ac.kr>
2023-05-11 20:18:03 +0900

    [UPD] @Hidden : ignore swagger ui

--- src/main/java/com/webdev/spring/controller/SampleJSONController.java
+++ src/main/java/com/webdev/spring/controller/SampleJSONController.java
@@ -1,10 +1,12 @@
 package com.webdev.spring.controller;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // API 서버 개발 시 사용하는 컨트롤러 애너테이션
+@Hidden // Swagger API 문서화시 해당 REST 컨트롤러 제외
 @Log4j2
 public class SampleJSONController {
 
