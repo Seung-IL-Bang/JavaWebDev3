309bba7da13e328057fb0cc4b267c89b1ee29eb5
Bang <seungilbang@khu.ac.kr>
2023-05-12 14:12:57 +0900

    [ADD] Reply 테스트

--- src/test/java/com/webdev/spring/repository/ReplyRepositoryTest.java
+++ src/test/java/com/webdev/spring/repository/ReplyRepositoryTest.java
@@ -2,16 +2,24 @@ package com.webdev.spring.repository;
 
 import com.webdev.spring.domain.Board;
 import com.webdev.spring.domain.Reply;
+import com.webdev.spring.dto.BoardListReplyCountDTO;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Log4j2
 public class ReplyRepositoryTest {
 
+    @Autowired
+    private BoardRepository boardRepository;
     @Autowired
     private ReplyRepository replyRepository;
 
@@ -30,5 +38,34 @@ public class ReplyRepositoryTest {
                 .build();
 
         replyRepository.save(reply);
+    }
+
+    @Test
+    public void testBoardReplies() {
+        Long bno = 100L;
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("rno").descending());
+
+        Page<Reply> result = replyRepository.listOfBoard(bno, pageable);
+
+        result.getContent().forEach(reply -> log.info(reply));
+    }
+
+    @Test
+    public void testSearchReplyCount() {
+        String[] types = {"t", "c", "w"};
+
+        String keyword = "1";
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<BoardListReplyCountDTO> result = boardRepository.searchWithReplyCount(types, keyword, pageable);
+
+        log.info(result.getTotalPages()); // total pages
+        log.info(result.getSize()); // page size
+        log.info(result.getNumber()); // page Number
+        log.info(result.hasPrevious() + ": " + result.hasNext()); // prev : next
+
+        result.getContent().forEach(board -> log.info(board));
     }
 }
---
+++ src/test/java/com/webdev/spring/service/ReplyServiceTest.java
@@ -0,0 +1,27 @@
+package com.webdev.spring.service;
+
+import com.webdev.spring.dto.ReplyDTO;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Log4j2
+public class ReplyServiceTest {
+
+    @Autowired
+    private ReplyService replyService;
+
+    @Test
+    public void testRegister() {
+
+        ReplyDTO replyDTO = ReplyDTO.builder()
+                .replyText("ReplyDTO Text")
+                .replyer("replyer")
+                .bno(200L)
+                .build();
+
+        log.info(replyService.register(replyDTO));
+    }
+}
