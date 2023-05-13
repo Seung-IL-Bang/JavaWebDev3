1fd9f65b1cde05f3a619d97c4a5227af81622545
Bang <seungilbang@khu.ac.kr>
2023-05-13 14:51:24 +0900

    [Feat] Reply CRUD through Axios

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -6,11 +6,45 @@ async function get1(bno) {
     return result.data
 }
 
+// 댓글 목록 출력
 async function getList({bno, page, size, goLast}) {
     const result = await axios.get(`/replies/list/${bno}`,
         {
             params:{page, size}
         })
 
+    if (goLast) {
+        const total = result.data.total
+        const lastPage = parseInt(Math.ceil(total/size))
+        return getList({bno:bno, page:lastPage, size:size})
+    }
+
     return result.data;
+}
+
+// 댓글 등록
+async function addReply(replyObj) {
+    const response = await axios.post(`/replies/`, replyObj)
+
+    return response.data
+}
+
+// 댓글 모달창에서 조회
+async function getReply(rno) {
+    const response = await axios.get(`/replies/${rno}`)
+
+    return response.data
+}
+
+// 댓글 모달창에서 수정
+async function modifyReply(replyObj) {
+    const response = await axios.put(`/replies/${replyObj.rno}`, replyObj)
+
+    return response.data
+}
+
+async function removeReply(rno) {
+    const response = await axios.delete(`/replies/${rno}`)
+
+    return response.data
 }
