ff6c47e6666f49674a8579e4020f1077c2219171
Bang <seungilbang@khu.ac.kr>
2023-05-15 22:29:30 +0900

    [UPD] 이미지 첨부파일 저장 테스트

--- src/test/java/com/webdev/spring/repository/BoardRepositoryTest.java
+++ src/test/java/com/webdev/spring/repository/BoardRepositoryTest.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Sort;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -124,4 +125,21 @@ public class BoardRepositoryTest {
 
         result.getContent().forEach(log::info);
     }
+
+    @Test
+    public void testInsertWithImages() {
+        Board board = Board.builder()
+                .title("Image Test")
+                .content("첨부파일 테스트")
+                .writer("tester")
+                .build();
+
+        for (int i = 0; i < 3; i++) {
+            board.addImage(UUID.randomUUID().toString(), "file" + i + ".jpg");
+        }
+
+        boardRepository.save(board);
+    }
+
+    
 }
