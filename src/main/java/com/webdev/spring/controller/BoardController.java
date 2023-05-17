110877d3a2cdb968878bc854d03696692a3fa118
Bang <seungilbang@khu.ac.kr>
2023-05-17 19:49:44 +0900

    [UPD] 게시판 목록 with all (not completed)

--- src/main/java/com/webdev/spring/controller/BoardController.java
+++ src/main/java/com/webdev/spring/controller/BoardController.java
@@ -1,13 +1,13 @@
 package com.webdev.spring.controller;
 
-import com.webdev.spring.dto.BoardDTO;
-import com.webdev.spring.dto.BoardListReplyCountDTO;
-import com.webdev.spring.dto.PageRequestDTO;
-import com.webdev.spring.dto.PageResponseDTO;
+import com.webdev.spring.dto.*;
 import com.webdev.spring.service.BoardService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,18 +16,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
+
 @Controller
 @RequestMapping("/board")
 @Log4j2
 @RequiredArgsConstructor
 public class BoardController {
 
+    @Value("${com.webdev.upload.path}")
+    private String uploadPath;
+
     private final BoardService boardService;
 
     @GetMapping("/list")
     public void list(PageRequestDTO pageRequestDTO, Model model) {
         //PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
-        PageResponseDTO<BoardListReplyCountDTO> responseDTO = boardService.listWithReplyCount(pageRequestDTO);
+        //PageResponseDTO<BoardListReplyCountDTO> responseDTO = boardService.listWithReplyCount(pageRequestDTO);
+
+        PageResponseDTO<BoardListAllDTO> responseDTO = boardService.listWithAll(pageRequestDTO);
 
         log.info(responseDTO);
 
@@ -101,13 +110,45 @@ public class BoardController {
     }
 
     @PostMapping("/remove")
-    public String remove(Long bno, RedirectAttributes redirectAttributes) {
+    public String remove(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
+        Long bno = boardDTO.getBno();
         log.info("remove post ..." + bno);
 
         boardService.remove(bno);
 
+        // 게시물이 데이터베이스상에서 삭제되었다면 첨부파일 삭제
+        log.info(boardDTO.getFileNames());
+        List<String> fileNames = boardDTO.getFileNames();
+
+        if (fileNames != null && fileNames.size() > 0) {
+            removeFiles(fileNames);
+        }
+
         redirectAttributes.addFlashAttribute("result", "removed");
 
         return "redirect:/board/list";
+    }
+
+    public void removeFiles(List<String> files) {
+        for (String fileName : files) {
+            Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+
+            String resourceName = resource.getFilename();
+
+            try {
+                String contentType = Files.probeContentType(resource.getFile().toPath());
+
+                resource.getFile().delete();
+
+                // 섬네일이 존재한다면
+                if (contentType.startsWith("image")) {
+                    File thumbnailFile = new File(uploadPath + File.separator + "s_" + fileName);
+
+                    thumbnailFile.delete();
+                }
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+        }
     }
 }
