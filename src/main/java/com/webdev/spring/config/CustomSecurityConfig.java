33b4ac6c35aafdd4bba5665da5f9324932aae024
Bang <seungilbang@khu.ac.kr>
2023-05-18 12:51:52 +0900

    [UPD] @EnableGlobalMethodSecurity is deprecated

--- src/main/java/com/webdev/spring/config/CustomSecurityConfig.java
+++ src/main/java/com/webdev/spring/config/CustomSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,7 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Log4j2
 @Configuration
 @RequiredArgsConstructor
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class CustomSecurityConfig {
 
 
